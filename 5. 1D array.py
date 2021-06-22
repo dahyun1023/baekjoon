222deb098cabdb5d566eae95159f06f25f23fe5d
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-06-22 19:32:53 +0900

    Update 5. 1D array.py

--- 5. 1D array.py
+++ 5. 1D array.py
@@ -1,9 +1,9 @@
 # min/max
-
 N   = int(input())
 N_list = list(map(int,input().split()))
 
 print(f'{min(N_list)} {max(N_list)}')
+
 
 # max value
 N_list = []
@@ -15,6 +15,7 @@ max_value = max(N_list)
 max_index = N_list.index(max_value)+1
 print(f'{max_value}\n{max_index}')
       
+    
  # How many numbers?
 a = 1
 for _ in range(3):
@@ -34,41 +35,49 @@ for i in range(10):
 r_list = []
 for _ in range(10):
     a = int(input())
-    r_list = append(r_list,a%42)
+    r_list.append(a%42)
 r_list = set(r_list)
-r_list = list(r_list) # print위한 list변형 필요?체크하자
 print(len(r_list))
+
+
 # average
 N = int(input())
-N_list = map(int, input().split())
+N_list = list( map(int, input().split()) )
+M = max(N_list)
+
+for i in range(len(N_list)):
+    N_list[i] = N_list[i]/M*100
 avr = sum(N_list)/N
 print(avr)
+
+
 # OX quiz
 N = int(input())
 for i in range(N):
-      score = 0
-      bonus = 0
-      result = input()
-     
-      for _ in range(len(result)):
-        if result[i] == 'O':
+    score = 0
+    bonus = 0
+    result = input()
+    for j in range(len(result)):
+        if result[j] == 'O':
             bonus += 1
             score = score + bonus
         else:
             bonus = 0
-      print(score)
+    print(score)
+    
+    
 # Am I better than the average?
 C = int(input())
 for i in range(C):
-      score_list = input().split()
-      score_list = list(score_list)
-      n = score_list[0]
-      ave = sum(score_list[1:])
-      good = 0
-      for j in range(n):
-          if score_list[j+1]>ave:
-              good += 1
-          else:
-              pass
-      a = good/n*100
-      print(f'{a:.3f}')
+    score_list = map(int, input().split())
+    score_list = list(score_list)
+    n    = score_list[0]
+    ave  = sum(score_list[1:])/n
+    good = 0
+    for j in range(n):
+        if score_list[j+1]>ave:
+            good += 1
+        else:
+            pass
+    a = good/n*100
+    print(f'{a:.3f}%')
