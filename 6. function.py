462f5fc6d6633e65a6e90258afe949375aca3233
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-06-22 23:05:37 +0900

    Update 6. function.py

--- 6. function.py
+++ 6. function.py
@@ -1,12 +1,9 @@
 # sum of N integers
-def fun_sum (a):
+def solve (a):
     sum = 0
     for i in range(len(a)):
         sum += a[i]
     return sum
-a = list(map(int, input()))
-print(fun_sum(a))
-
 
 # self number
 def self_num(a):
@@ -18,10 +15,33 @@ def self_num(a):
         a = int(a/10)
     return result
 
-for i in range(1,10*4+1):
-    if self_num(i)==i:
-        print(i)
+N_list = []
+for i in range(1,10000+1):
+    N_list.append(self_num(i))
+N_list = list(set(N_list) )
+a      = list(range(1,10000+1))
+
+for i in range(0,100000):
+    x = N_list[i]
+    if x > 10000:
+        break
     else:
-        pass
+        a.remove(x)
+
+for i in range(len(a)):
+    print(a[i])
+# self number (better solution) 
+# https://github.com/wook2124/Algorithm-Test/tree/master/%EB%B0%B1%EC%A4%80%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/%EA%B5%AC%ED%98%84
+natural = set(range(1,10000))
+self_num= set()
+
+for i in range(1,1001):
+    for j in str(i):
+        i += int(j)
+    self_num.add(i)
+self_num = sorted(natural - self_num)
+
+for i in self_num:
+    print(i)
     
 # 한수
