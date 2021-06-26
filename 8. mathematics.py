e6ea5a64655499517b2de8beddf6cc26377c9717
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-06-26 07:15:45 -0700

    0626

--- 8. mathematics.py
+++ 8. mathematics.py
@@ -5,3 +5,44 @@ if B >= C:
 else:
     n = A//(C-B) + 1
     print(n)
+
+    
+# honey-bee house
+N = int(input())
+
+if N == 1:
+    print(N)
+else:  
+    sum = 1
+    i   = 1
+    while True:
+        a    = i*6
+        sum += a
+        if sum>=N:
+            print(i+1)
+            break
+        i += 1
+        
+        
+# numerator/denominator
+
+N   = int(input())
+i   = 2
+tot = 2
+while True:
+    if N<i:
+        j = i-N
+        if tot%2 == 1:
+            denominator = j
+            numerator   = tot-j
+        else:
+            denominator = tot-j
+            numerator   = j
+        print(f'{numerator}/{denominator}')
+        break
+    else:
+        i += tot
+    tot += 1
+    
+    
+# snail wants to go up
