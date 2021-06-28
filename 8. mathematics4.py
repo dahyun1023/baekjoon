da567611d88c9d5d76e17a9aa2fb2512c0187c89
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-06-28 12:14:41 -0700

    0629

--- 8. mathematics4.py
+++ 8. mathematics4.py
@@ -1,4 +1,6 @@
-# Fly me to the alpha centauri
+# Fly me to the alpha centauri 
+
+# My solution (976ms)
 T   = int(input())
 for _ in range(T):
     x,y = map(int, input().split())
@@ -19,3 +21,16 @@ for _ in range(T):
             diff  -= 2*num
             count += 2        
             num   += 1
+
+ # solibaht solution(68ms)
+import sys
+import math
+k = int(input())
+for i in range(k):
+  A, B = map(int,sys.stdin.readline().split())
+  n = B - A
+  a = int(math.sqrt(n - 1)) + 1
+  if n > a * (a - 1):
+    print(2 * a - 1)
+  else:
+    print(2 * a - 2)
