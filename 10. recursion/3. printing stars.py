028b186b076432a4c5f2cb4a6b9c8c29ae02dcf3
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-07-11 07:08:50 -0700

    Update 3. printing stars.py

--- 10. recursion/3. printing stars.py
+++ 10. recursion/3. printing stars.py
@@ -1,6 +1,34 @@
-N = int(input())
+# Code review
+#https://study-all-night.tistory.com/5 
+    
+def draw_star(n) :
+    global Map
+    
+    if n == 3 :
+        Map[0][:3] = Map[2][:3] = [1]*3
+        Map[1][:3] = [1, 0, 1]
+        return
 
-def basic(k):
-    print('***'*3**(k-1))
-    print(f'* *'*3**(k-1))
-    print('***'*3**(k-1))
+    a = n//3
+    draw_star(n//3)
+    for i in range(3) :
+        for j in range(3) :
+            if i == 1 and j == 1 :
+                continue
+            for k in range(a) :
+                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a] # 핵심 아이디어
+
+N = int(input())      
+
+# 메인 데이터 선언
+Map = [[0 for i in range(N)] for i in range(N)] 
+
+draw_star(N)
+
+for i in Map :
+    for j in i :
+        if j :
+            print('*', end = '')
+        else :
+            print(' ', end = '')
+    print()
